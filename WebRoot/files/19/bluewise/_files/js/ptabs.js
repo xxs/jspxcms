73d3f0b4344a9b3e5238fa128ded3d03c1a0c19f
朵儿 <xiaoshi332@163.com>
2016-05-21 08:39:24 +0800

    保存新模板

---
+++ WebRoot/files/19/bluewise/_files/js/ptabs.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+var option={
+		tabs:".tabs",
+		tl:"li",
+		onClass:"on",
+		tabsc:".tabcont",
+		tabc:".tabc",
+		e:"click"
+}
+//<li rev="{e:'ajax',u:'get.asp'}">
+var pTabs={
+	run:function(arg){
+		var tabs,tl,onClass,tabsc,tabc,e;
+		tabs=arg.tabs || option.tabs;
+		tl=arg.tl || option.tl;
+		onClass=arg.onClass || option.onClass;
+		tabsc=arg.tabsc || option.tabsc;
+		tabc=arg.tabc || option.tabc;
+		e=arg.e || option.e;
+		
+		$(tl,$(tabs)).each(function(i){
+			$(tl,$(tabs)).first().addClass(onClass)
+			$(tabc,$(tabsc)).first().siblings().hide()
+			$(this).bind(e, function() {
+				$(this).addClass(onClass).siblings().removeClass();
+				$(tabc,$(tabsc)).eq(i).show().siblings().hide()
+				if($(this).attr("event")!=undefined){
+					switch($(this).attr("event")){
+					case "ajax":
+						$(tabc,$(tabsc)).eq(i).html($(this).attr("url"))
+					break;
+					case "text":
+						$(tabc,$(tabsc)).eq(i).html($(this).attr("text"))
+					break;
+					}
+				}
+				
+				//alert($(tabc,$(tabsc)).is(":visible"))
+				
+			});
+		})
+	}
+}
+
+/*
+—°‘Ò∂‘œÛ.parent.redirect("")
+
+
+function redirect(url){
+		
+}
+*/
---
+++ WebRoot/files/19/bluewise/_files/js/turn.js
@@ -0,0 +1,19 @@
+
+    $(function () {
+        $(".cCases li").hover(function () {
+            $(this).addClass("hover");
+        }, function () {
+            $(this).removeClass("hover");
+        })
+    })
+
+    $(function () {
+        $(".cproduct li").hover(function () {
+            $(this).addClass("hover");
+        }, function () {
+            $(this).removeClass("hover");
+        })
+    })
+	
+	
+
